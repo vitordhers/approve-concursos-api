d61fca89782c1975c4b31a694c34171e32a2ed2a
Vitor Dhers <vitor.dhers@gmail.com>
2024-01-29 20:35:32 -0300

    removes unused guards from AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,12 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AdminRoleStrategy } from './guards/admin-role.strategy';
-import { LocalStrategy } from './guards/local.strategy';
 import { GoogleRecaptchaV3Constraint } from '../shared/validators/google-recaptcha-v3.constraint';
 import { TokensModule } from '../tokens/tokens.module';
 import { AccessTokenStrategyConfigFactory } from './constants/access-token-config.const';
 import { RefreshTokenStrategy } from './guards/refresh-token.strategy';
 import { RefreshTokenStrategyConfigFactory } from './constants/refresh-token-config.const';
 import { SerializationModule } from 'src/serialization/serialization.module';
-import { AtLeastVerifiedUserStrategy } from './guards/at-least-verified-user.strategy';
-import { PaidUserStrategy } from './guards/paid-user.strategy';
 import { EmailsModule } from 'src/email/email.module';
 
 @Module({
@@ -21,7 +17,7 @@ import { EmailsModule } from 'src/email/email.module';
     UsersModule,
     HttpModule,
     PassportModule.register({
-      defaultStrategy: ['accessToken', 'refreshToken', 'adminToken'],
+      defaultStrategy: ['accessToken', 'refreshToken'],
     }),
     TokensModule,
     SerializationModule,
@@ -32,12 +28,7 @@ import { EmailsModule } from 'src/email/email.module';
     AuthService,
     AccessTokenStrategyConfigFactory,
     RefreshTokenStrategyConfigFactory,
-    // AccessTokenStrategy,
-    AtLeastVerifiedUserStrategy,
-    PaidUserStrategy,
     RefreshTokenStrategy,
-    AdminRoleStrategy,
-    LocalStrategy,
     GoogleRecaptchaV3Constraint,
   ],
   exports: [AuthService, GoogleRecaptchaV3Constraint],
